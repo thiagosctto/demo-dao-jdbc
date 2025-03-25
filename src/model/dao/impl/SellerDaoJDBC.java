c433b603028e094e697d200cbe4f1f25e3371fd2
thiagosctto <tiago.games1000@gmail.com>
2025-03-25 18:50:08 -0300

    Implementando update

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -25,6 +25,7 @@ public class SellerDaoJDBC implements SellerDao{
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
+//=================================
 
 	@Override
 	public void insert(Seller obj) {
@@ -64,12 +65,34 @@ public class SellerDaoJDBC implements SellerDao{
 		}
 				
 	}
+//=================================
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;
 		
+		try {
+			st = conn.prepareStatement(
+					"UPDATE seller "
+					+"SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+"WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+		}catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}finally {
+			DB.closeStatement(st);
+			
+		}
 	}
+//=================================
 
 	@Override
 	public void deleteById(Integer id) {
